75675cfbe72e883aa8a482455ec8aaecf6f5c05a
Jimi Patel <pjimi1746@gmail.com>
2026-09-20 21:50:10 +0530

    Time: 0 ms (100%), Space: 9.8 MB (47.88%) - LeetHub

---
+++ 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int reverseDegree(string s) {
+        int count=0;
+        for(int i=0;i<s.length();i++)
+        {
+            count  += (i+1)*(26-s[i]+97);
+        }
+        return count;
+    }
+};
